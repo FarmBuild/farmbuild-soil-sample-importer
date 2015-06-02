ea4f8716922395f4285056cccff4844eb7be9380
kxw <ken.wonoatmojo@spatialvision.com.au>
2015-06-02 12:34:14 +1000

    clean up code

--- src/paddock-selector/index.src.js
+++ src/paddock-selector/index.src.js
@@ -14,122 +14,6 @@ angular.module('farmbuild.soilSampleImporter')
               collections, soilSampleConverter, paddockSelectionValidator) {
         $log.info("paddockSelector ");
 
-        var myFarmData = {
-            "version": 1,
-            "dateCreated": "2015-03-30T21:19:00",
-            "dateLastUpdated": "2015-05-18T23:40:28.764Z",
-            "name": "Susan's Farm",
-            "geometry": {
-                "type": "Polygon",
-                "crs": "EPSG:4283",
-                "coordinates": [
-                    [
-                        [
-                            145.57368096419663,
-                            -36.224879531701255
-                        ],
-                        [
-                            145.5826132801325,
-                            -36.22488327137526
-                        ],
-                        [
-                            145.58260951039628,
-                            -36.22801228957186
-                        ],
-                        [
-                            145.57363088613704,
-                            -36.22803939355771
-                        ],
-                        [
-                            145.57368096419663,
-                            -36.224879531701255
-                        ]
-                    ]
-                ]
-            },
-            "area": 89.95,
-            "areaUnit": "hectare",
-            "paddocks": [
-                {
-                    "name": "P3",
-                    "geometry": {
-                        "type": "Polygon",
-                        "crs": "EPSG:4283",
-                        "coordinates": [
-                            [
-                                [
-                                    145.58072144612308,
-                                    -36.22500072918365
-                                ],
-                                [
-                                    145.58007203644695,
-                                    -36.224996785309635
-                                ],
-                                [
-                                    145.58007068632003,
-                                    -36.22579563889769
-                                ],
-                                [
-                                    145.579975038195,
-                                    -36.22579611106869
-                                ],
-                                [
-                                    145.57997732422797,
-                                    -36.22610013854838
-                                ],
-                                [
-                                    145.58069096429173,
-                                    -36.226090652211994
-                                ],
-                                [
-                                    145.58072144612308,
-                                    -36.22500072918365
-                                ]
-                            ]
-                        ]
-                    },
-                    "area": 0.72
-                },
-                {
-                    "name": "P1",
-                    "geometry": {
-                        "type": "Polygon",
-                        "crs": "EPSG:4283",
-                        "coordinates": [
-                            [
-                                [
-                                    145.5820357180293,
-                                    -36.224883050102875
-                                ],
-                                [
-                                    145.58072609426455,
-                                    -36.22488253784964
-                                ],
-                                [
-                                    145.58070132605394,
-                                    -36.225512331175615
-                                ],
-                                [
-                                    145.58218018756546,
-                                    -36.22550501283936
-                                ],
-                                [
-                                    145.58211312173296,
-                                    -36.225216285770216
-                                ],
-                                [
-                                    145.5820357180293,
-                                    -36.224883050102875
-                                ]
-                            ]
-                        ]
-                    },
-                    "area": 0.87
-                }
-            ]
-        }
-
-
         var paddockSelector = {},
             _paddocks = [],
             _types = soilClassificationTypes.toArray();
@@ -159,7 +43,7 @@ angular.module('farmbuild.soilSampleImporter')
         }
 
 
-        paddockSelector.load = function() {
+        /*paddockSelector.load = function() {
             var test= {
                 "dateLastUpdated": "2015-05-25T02:23:51",
                 "columnHeaders" : [
@@ -178,7 +62,8 @@ angular.module('farmbuild.soilSampleImporter')
                 "paddockColumnIndex":0
             };
             return test;
-        }
+        }*/
+
         paddockSelector.save = function(paddockSelection) {
             $log.info(JSON.stringify(paddockSelection));
 
@@ -217,7 +102,6 @@ angular.module('farmbuild.soilSampleImporter')
         }
 
         paddockSelector.selectColumn =  function(paddockSelection, value) {
-            $log.info("adding "+value+ " to "+ paddockSelection);
             collections.add(paddockSelection.selected, value);
         }
 
