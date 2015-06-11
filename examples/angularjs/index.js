8d51ea8d7dd71ff029d0e80adec8c3a1f4eaf886
kxw <ken.wonoatmojo@spatialvision.com.au>
2015-06-11 13:15:36 +1000

    reformat example code... no change

--- examples/angularjs/index.js
+++ examples/angularjs/index.js
@@ -1,84 +1,80 @@
 angular.module('farmbuild.soilSampleImporter.examples', ['farmbuild.soilSampleImporter'])
 
-  .run(function ($rootScope) {
-    $rootScope.appVersion = farmbuild.examples.soilsampleimporter.version;
-    $rootScope.decimalPrecision = farmbuild.examples.soilsampleimporter.decimalPrecision;
-  })
+    .run(function ($rootScope) {
+        $rootScope.appVersion = farmbuild.examples.soilsampleimporter.version;
+        $rootScope.decimalPrecision = farmbuild.examples.soilsampleimporter.decimalPrecision;
+    })
 
-  .controller('FarmCtrl', function ($scope, $log, soilSampleImporter) {
+    .controller('FarmCtrl', function ($scope, $log, soilSampleImporter) {
 
-//    var load = false;
-//    if (location.href.split('?').length > 1 && location.href.split('?')[1].indexOf('load') === 0) {
-//      load = (location.href.split('?')[1].split('=')[1] === 'true');
-//    }
 
-    $scope.farmData = {};
-
-    $scope.loadFarmData = function ($fileContent) {
-      $log.info('FarmCtrl>>loadFarmData>>start');
-
-      try {
         $scope.farmData = {};
-        var farmData = soilSampleImporter.load(angular.fromJson($fileContent));
-        $log.info('FarmCtrl>>loadFarmData');
-        if (!angular.isDefined(farmData)) {
-          $scope.noResult = true;
-          return;
+
+        $scope.loadFarmData = function ($fileContent) {
+            $log.info('FarmCtrl>>loadFarmData>>start');
+
+            try {
+                $scope.farmData = {};
+                var farmData = soilSampleImporter.load(angular.fromJson($fileContent));
+                $log.info('FarmCtrl>>loadFarmData');
+                if (!angular.isDefined(farmData)) {
+                    $scope.noResult = true;
+                    return;
+                }
+
+                //getSampleResultsTable(farmData);
+                updateFarmData($scope, farmData);
+                $scope.prettyContent = JSON.stringify(farmData, null, "    ");
+            } catch (e) {
+                console.error('farmbuild.soilSampleImporter.examples > load: Your file should be in json format: ', e);
+                $scope.noResult = true;
+            }
+        };
+
+        $scope.exportFarmData = function (farmData) {
+            var url = 'data:application/json;charset=utf8,' + encodeURIComponent(JSON.stringify(farmData, undefined, 2));
+            window.open(url, '_blank');
+            window.focus();
+        };
+
+
+        $scope.calculate = function () {
+            $log.info('calculate...');
+
+            updateFarmData($scope, farmData);
+
+            soilSampleImporter.ga.trackCalculate('AgSmart');
+        };
+
+        $scope.clear = function () {
+            $scope.farmData = {};
+            soilSampleImporter.farmdata.session.clear();
+            var path = location.href.toString(),
+                path = path.substring(0, path.indexOf('?'));
+            location.href = path;
         }
 
-        //getSampleResultsTable(farmData);
-        updateFarmData($scope, farmData);
-        $scope.prettyContent = JSON.stringify(farmData,null,"    ");
-      } catch (e) {
-        console.error('farmbuild.soilSampleImporter.examples > load: Your file should be in json format: ', e);
-        $scope.noResult = true;
-      }
-    };
+        if (soilSampleImporter.session.isLoadFlagSet(location)) {
+            var farmData = soilSampleImporter.find();
 
-    $scope.exportFarmData = function (farmData) {
-      var url = 'data:application/json;charset=utf8,' + encodeURIComponent(JSON.stringify(farmData, undefined, 2));
-      window.open(url, '_blank');
-      window.focus();
-    };
+            updateFarmData($scope, farmData);
+        }
 
-
-
-    $scope.calculate = function () {
-      $log.info('calculate...');
-
-      updateFarmData($scope, farmData);
-
-      soilSampleImporter.ga.trackCalculate('AgSmart');
-    };
-
-    $scope.clear = function () {
-      $scope.farmData ={};
-      soilSampleImporter.farmdata.session.clear();
-      var path = location.href.toString(),
-        path = path.substring(0, path.indexOf('?'));
-      location.href = path;
-    }
-
-    if (soilSampleImporter.session.isLoadFlagSet(location)) {
-      var farmData = soilSampleImporter.find();
-
-      updateFarmData($scope, farmData);
-    }
-
-    function updateFarmData($scope, farmData) {
-      if(!farmData) {
-        $log.error('Failed to load milkSold data...');
-        $scope.noResult = true;
-        return;
-      }
-      $scope.farmData = farmData;
+        function updateFarmData($scope, farmData) {
+            if (!farmData) {
+                $log.error('Failed to load milkSold data...');
+                $scope.noResult = true;
+                return;
+            }
+            $scope.farmData = farmData;
 //      $scope.balance = farmData.soilSampleImporter.balance;
 //      $scope.efficiency = farmData.soilSampleImporter.efficiency;
 //      $scope.feedBalance = farmData.soilSampleImporter.feedBalance;
 //      $scope.milkProduction = farmData.soilSampleImporter.milkProduction;
 //      $scope.stockingRate = farmData.soilSampleImporter.stockingRate;
 
-    }
+        }
+
 //    $scope.file;
 //
 //    $scope.loadFile =  function (file) {
@@ -121,37 +117,37 @@ angular.module('farmbuild.soilSampleImporter.examples', ['farmbuild.soilSampleIm
 ////      reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
 //    };
 
-  })
+    })
 
-  .directive('onReadFile', function ($parse, $log) {
-    return {
-      restrict: 'A',
-      scope: false,
-      link: function (scope, element, attrs) {
-        var fn = $parse(attrs.onReadFile);
+    .directive('onReadFile', function ($parse, $log) {
+        return {
+            restrict: 'A',
+            scope: false,
+            link: function (scope, element, attrs) {
+                var fn = $parse(attrs.onReadFile);
 
-        element.on('change', function (onChangeEvent) {
-          //var file =  (onChangeEvent.srcElement || onChangeEvent.target).files[0]
-          var file =  (onChangeEvent.target).files[0]
-          $log.info('onReadFile.onChange... onChangeEvent.srcElement:%s, ' +
-              'onChangeEvent.target:%s, (onChangeEvent.srcElement || onChangeEvent.target).files[0]: %s',
-            onChangeEvent.srcElement, onChangeEvent.target,
-            angular.toJson(file))
+                element.on('change', function (onChangeEvent) {
+                    //var file =  (onChangeEvent.srcElement || onChangeEvent.target).files[0]
+                    var file = (onChangeEvent.target).files[0]
+                    $log.info('onReadFile.onChange... onChangeEvent.srcElement:%s, ' +
+                        'onChangeEvent.target:%s, (onChangeEvent.srcElement || onChangeEvent.target).files[0]: %s',
+                        onChangeEvent.srcElement, onChangeEvent.target,
+                        angular.toJson(file))
 
-          var reader = new FileReader();
+                    var reader = new FileReader();
 
-          reader.onload = function (onLoadEvent) {
-            //console.log('reader.onload', angular.toJson(onLoadEvent));
-            scope.$apply(function () {
-              fn(scope, {$fileContent: onLoadEvent.target.result});
-            });
-          };
-          reader.onerror = function (onLoadEvent) {
-            //console.log('reader.onload', angular.toJson(onLoadEvent));
-          };
+                    reader.onload = function (onLoadEvent) {
+                        //console.log('reader.onload', angular.toJson(onLoadEvent));
+                        scope.$apply(function () {
+                            fn(scope, {$fileContent: onLoadEvent.target.result});
+                        });
+                    };
+                    reader.onerror = function (onLoadEvent) {
+                        //console.log('reader.onload', angular.toJson(onLoadEvent));
+                    };
 
-          reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
-        });
-      }
-    };
-  });
+                    reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+                });
+            }
+        };
+    });
