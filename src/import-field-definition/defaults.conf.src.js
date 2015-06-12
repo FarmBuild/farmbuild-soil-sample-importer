5d859ff6ab8f78e7f5d9035d6afb7ee12201f10f
kxw <ken.wonoatmojo@spatialvision.com.au>
2015-06-12 15:57:50 +1000

    add Exch Potassium (meq/100g)

--- src/import-field-definition/defaults.conf.src.js
+++ src/import-field-definition/defaults.conf.src.js
@@ -8,6 +8,7 @@ angular.module('farmbuild.soilSampleImporter').
             {"name": "PBI", "soilClassificationName" : "PBI", "hasAverage":true},
             {"name": "KCl 40 Sulphur (mg/kg)", "soilClassificationName" : "KCl 40 Sulphur (mg/kg)", "hasAverage":true},
             {"name": "Colwell Phosphorus (mg/kg)", "soilClassificationName" : "Colwell Phosphorus (mg/kg)", "hasAverage":true},
-            {"name": "Colwell Potassium (mg/kg)", "soilClassificationName" : "Colwell Potassium (mg/kg)", "hasAverage":true}
+            {"name": "Colwell Potassium (mg/kg)", "soilClassificationName" : "Colwell Potassium (mg/kg)", "hasAverage":true},
+            {"name": "Exch Potassium (meq/100g)", "soilClassificationName" : "Exch Potassium (meq/100g)", "hasAverage":true}
         ]
     });
